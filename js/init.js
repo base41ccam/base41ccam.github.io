e4936790d59b9f8a068056d5239c7ca94e3835f8
base41ccam <base41ccam@gmail.com>
2023-03-29 19:12:31 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyDMgdWAS7eroWySBQcremr4KLifcQPf5Lg",
+    authDomain: "base41ccam1-301db.firebaseapp.com",
+    projectId: "base41ccam1-301db",
+    storageBucket: "base41ccam1-301db.appspot.com",
+    messagingSenderId: "47144205524",
+    appId: "1:47144205524:web:bd41aa7a226fb02e9fcf41",
+    measurementId: "G-5Q1MR9606F"
+});
